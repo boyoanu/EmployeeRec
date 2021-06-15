e724f616a79c5d883aa1d411964e8fb956f8fd79
boyoanu <85933732+boyoanu@users.noreply.github.com>
2021-06-15 11:08:57 +0100

    Add files via upload

---
+++ EmployeeRec/Hr/Employee.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRec.Hr
+{
+    public class Employee // Declaration of class Employee
+    {
+        private readonly int _staffId;
+        private string _jobTitle;
+        private readonly string _fullName;
+        private readonly string _employeeDate;
+        private string _department;
+
+
+        // Contructing a Contructor Employee
+        public Employee(int staffId, string jobTitle, string fullName, string employeeDate, string department)
+
+        {
+            _staffId = staffId;
+            _jobTitle = jobTitle;
+            _fullName = fullName;
+            _employeeDate = employeeDate;
+            _department = department;
+
+        }
+
+        public int StaffId
+        {
+            get => _staffId; // Getting a value readonly
+        }
+
+        public string JobTitle
+        {
+            get => _jobTitle;
+        
+       
+             set 
+            {
+                _jobTitle = value;
+            }
+        }
+        public string Name
+        {
+            get => _fullName; // Getting a value readonly
+        }
+
+        public string EmployeeDate
+
+        // This get accessor users the explicit syntyax, rather than a short form
+        {
+            get => _employeeDate;   
+        }
+
+        public string Department
+        {
+            get => _department;
+
+            set
+            {
+                _department = value;
+            }
+
+
+        }
+
+
+        public void ClockIn() 
+        {
+            Console.WriteLine("Employee ClockIn Time is : '8:00AM'");
+            
+        }
+        
+         
+        
+        public void ClockOut() 
+        {
+            Console.WriteLine("Employee ClockOut Time is : '5:00PM'");
+        }
+
+        public void Work() 
+        {
+            Console.WriteLine("Employee work for a Full Day ! '");
+        }
+        
+        
+          
+       
+     
+        
+    }
+}
---
+++ EmployeeRec/Program.cs
@@ -0,0 +1,80 @@
+﻿using EmployeeRec.Hr;
+using System;
+
+namespace EmployeeRec
+{
+    class program
+    {
+
+
+        static void Main(string[] args)
+
+        {
+
+
+            // Creating 5 employees
+            Employee firstEmployee = new Employee(101, "Tunde Badmus", "Accountant", "06/06/2021", "Account Department");
+            Employee secondEmployee = new Employee(102, "Phillip Newman", "Account Clerk", "06/06/2021", "Accoun Department");
+            Employee thirdEmployee = new Employee(103, "John Doe", "CFO", "01/01/2021", "Account Department,");
+            Employee forthEmployee = new Employee(104, "Betty Sandy", "IT Student", "06/05/2021", "Information Technology");
+            Employee fifthEmployee = new Employee(105, "Samuel Stone", "IT Admin", "05/02/2021", "Information Technology");
+
+
+            // Give each employee a different job title.
+
+            firstEmployee.JobTitle = "Accountant";
+            secondEmployee.JobTitle = "Account Clerk";
+            thirdEmployee.JobTitle = "CFO";
+            forthEmployee.JobTitle = "IT Student";
+            fifthEmployee.JobTitle = "IT Admin";
+
+            // Assign 3 employee the same department, and the other 2 a differnt department.
+
+            firstEmployee.JobTitle = "Account Department";
+            secondEmployee.JobTitle = "Account Department";
+            thirdEmployee.JobTitle = "Account Department";
+            forthEmployee.JobTitle = "Information Technology";
+            fifthEmployee.JobTitle = "Information Technology";
+
+            // Store all 5 employees in an array of type Employee
+
+
+            Employee[] Employees = new Employee[5]
+            {
+                new Employee(101, "Tunde Badmus", "Accountant", "06/06/2021", "Account Department"),
+                new Employee(102, "Phillip Newman", "Account Clerk", "06/06/2021", "Accoun Department"),
+                new Employee(103, "John Doe", "CFO", "01/01/2021", "Account Department,"),
+                new Employee(104, "Betty Sandy", "IT Student", "06/05/2021", "Information Technology"),
+                new Employee(105, "Samuel Stone", "IT Admin", "05/02/2021", "Information Technology"),
+
+            };
+
+            Console.WriteLine("Employee Record");
+            Console.WriteLine("---------------------------");
+
+            foreach (Employee Employee in Employees)
+            {
+
+                Console.WriteLine("SaffId: {0}", Employee.StaffId);
+                Console.WriteLine("FullName:  {0}", Employee.Name);
+                Console.WriteLine("JobTitle:     {0}", Employee.JobTitle);
+                Console.WriteLine("EmployeeDate: {0}", Employee.EmployeeDate);
+                Console.WriteLine("Department: {0}", Employee.Department);
+                Employee.ClockIn();
+                Employee.ClockOut();
+                Employee.Work();
+                Console.WriteLine("---------------------------\n");
+               
+
+                
+            }
+
+
+
+            
+
+        }
+    }
+}
+    
+
